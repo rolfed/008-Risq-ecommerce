3150aed37e0a1146b53c9656ce48d3e17b3c1f91
Danniel S Rolfe <info@dannielrolfe.com>
2016-11-30 14:41:58 -0800

    Added schema to users and purchases table

--- database/migrations/2016_11_30_223227_create_purchases_table.php
+++ database/migrations/2016_11_30_223227_create_purchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePurchasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('purchases', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            //$table->rememberToken();
-            $table->string('stripe_customer_id')->unique();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('product');
+            $table->integer('amount');
+            $table->string('stripe_transaction_id');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('purchases');
     }
 }
