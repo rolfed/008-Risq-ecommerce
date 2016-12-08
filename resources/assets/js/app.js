db93124fe6f95c4f270bcbda8a9a689a85bf4302
Danniel S Rolfe <info@dannielrolfe.com>
2016-12-08 11:29:15 -0800

    Mobile csrf token fixed

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,8 +4,9 @@ var app = angular.module('risq', [
 	'uiGmapgoogle-maps',
 	'angular-stripe'
 ])
-.config(function (stripeProvider) {
+.config(function (stripeProvider, uiGmapGoogleMapApiProvider) {
     stripeProvider.setPublishableKey("pk_test_bUH3O6nDbsq0olDgE7h9L2fv");
+	uiGmapGoogleMapApiProvider.configure({ key: 'AIzaSyAzN29aDknykQyTUmAWJapI_z_Mu7wKv3Q' });
  });
 
 
--- resources/assets/js/directives/orderMobile.directive.js
+++ resources/assets/js/directives/orderMobile.directive.js
@@ -4,41 +4,43 @@ function orderMobile(){
 	var directive = {
 		link: linkFunc,
 		templateUrl: '../assets/views/_partials/order.html',
-		$scope: {
+		scope: {
 			csrfTokenMobile : '@',
 			objTotal : '@'
 		},
         restrict: 'E'
 	}
 
+
+
 	/* DOM Animation */
 	function linkFunc($scope){
-		// /* Stripe payment */
-  //       var $form = $('#payment-form');
-  //       //console.log($form);
+		/* Stripe payment */
+        var $form = $('#payment-form');
+        //console.log($form);
 
-  //       function stripeResponseHandler(status, response) {
-  //           if (response.error) {
-  //               $form.find('.payment-errors').text(response.error.message);
-  //               $form.find('#submit-form').prop('disabled', false);
-  //           } else {
-  //               var token = response.id;
-  //               $form.append($('<input type="hidden" name="stripeToken">').val(token));
-  //               $form.find('#submit-form').prop('disabled', false);
-  //               $form.get(0).submit();
-  //           }
-  //       };
+        function stripeResponseHandler(status, response) {
+            if (response.error) {
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('#submit-form').prop('disabled', false);
+            } else {
+                var token = response.id;
+                $form.append($('<input type="hidden" name="stripeToken">').val(token));
+                $form.find('#submit-form').prop('disabled', false);
+                $form.get(0).submit();
+            }
+        };
 
-  //       $form.submit(function(event) {
-  //           $form.find('#submit-form').prop('disabled', true);
-  //           Stripe.card.createToken($form, stripeResponseHandler);
-  //           return false;  
-  //       });
-
-        console.log($scope.csrfTokenMobile, $scope);
+        $form.submit(function(event) {
+            $form.find('#submit-form').prop('disabled', true);
+            Stripe.card.createToken($form, stripeResponseHandler);
+            return false;  
+        });
 
 	}
 
 	/* Load Directive */
 	return directive;
 }; 
+
+
