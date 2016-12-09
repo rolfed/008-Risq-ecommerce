f290c71c35844557496a8045395bd8713e38fcde
Danniel S Rolfe <info@dannielrolfe.com>
2016-12-08 18:14:11 -0800

    All back btns work

--- resources/assets/js/directives/ecom.directive.js
+++ resources/assets/js/directives/ecom.directive.js
@@ -18,7 +18,7 @@ function ecom(){
 	View 4 is the address form. View 5 is the credit card form. View 6 is
 	the products have shipped view */
 	
-	function linkFunc($scope) {
+	function linkFunc($scope, $event) {
 		
 		/* Stripe payment */
         var $form = $('#payment-form');
@@ -112,7 +112,61 @@ function ecom(){
 		};
 
 		/* Form Validation */
-		$scope.addrValidation = function(checkout){
+		$scope.addrValidation = function($event){
+			var firstName = document.forms['payment-form']['first-name'].value;
+			var lastName = document.forms['payment-form']['last-name'].value;
+			var addr1 = document.forms['payment-form']['address-line-1'].value;
+			var city = document.forms['payment-form']['city'].value;
+			var zip = document.forms['payment-form']['zip'].value;
+			var state = document.forms['payment-form']['state'].value;
+			var email = document.forms['payment-form']['email'].value;
+			var errorMsg = document.getElementById('errorMessage');
+			var btn = document.getElementById('payment-animation-button');
+
+			if(	firstName == null || lastName  ==  null || addr1 ==  null || city  ==  null || zip  ==  null || state  ==  null || state  ==  "State" || email  ==  null){
+				$event.stopPropagation();
+				errorMsg.className = 'fadeIn red';	
+			};
+
+		};
+
+		/* Address Form - View 3 to View 4 */
+		$scope.checkout = function(){
+			var view2 = document.querySelector('#view-2');
+			var view3 = document.querySelector('#view-3');
+			var view4 = document.querySelector('#view-4');
+			view2.style.left="100%";
+			view3.style.right="-100%";
+			view3.style.opacity="0";
+			view4.style.right="0";
+		};
+
+		/* Back Button - Payment Form - View 5 to View 4 */
+		/*************************************************/
+		$scope.backToAddress = function(){
+			/* Animation Variables */
+			var view2 = document.querySelector('#view-2');
+			var view4 = document.querySelector('#view-4');
+			var view5 = document.querySelector('#view-5');
+
+			view5.style.left="100%";
+			view4.classList.toggle('hidden');
+			view4.style.right="0";
+			view4.style.opacity="1";
+			// view4.style.opacity="1";
+				
+			// 	view2.classList.toggle('hidden');
+
+			// 	setTimeout(function(){ 
+			// 		view4.classList.toggle('hidden');
+			// 		view5.style.right="1";
+			// 		//view5.classList.toggle('hidden');
+			// 	}, 100);
+		};
+
+		/* Payment Form - View 4 to View 5 */
+		$scope.paymentInput = function(){
+			/* Form Inputs */
 			var firstName = document.forms['payment-form']['first-name'].value;
 			var lastName = document.forms['payment-form']['last-name'].value;
 			var addr1 = document.forms['payment-form']['address-line-1'].value;
@@ -121,50 +175,30 @@ function ecom(){
 			var state = document.forms['payment-form']['state'].value;
 			var email = document.forms['payment-form']['email'].value;
 
-			if(	firstName == null || lastName  ==  null || addr1 ==  null || city  ==  null || zip  ==  null || state  ==  null || state  ==  "State" || email  ==  null){
-				alert('Please fill out all fields');
-
-				return checkout.preventDefault();
-			};
-
-		};
-
-		/* Address Form - View 3 to View 4 */
-		$scope.checkout = function(){
-			//console.log("addrInput is loading");
-			
-
-			var view2 = document.querySelector('#view-2');
-			var view3 = document.querySelector('#view-3');
-			var view4 = document.querySelector('#view-4');
-
-			view2.style.left="100%";
-			view3.style.right="-100%";
-			view3.style.opacity="0";
-			view4.style.right="0";
-
-		};
-
-
-
-		/* Payment Form - View 4 to View 5 */
-		$scope.paymentInput = function(){
+			/* Error message */
+			var errorMsg = document.getElementById('errorMessage');
+			var btn = document.getElementById('payment-animation-button');
 			var inputFields = document.getElementsByClassName('ecom-form-selector');
-
-			//console.log('Add Payment Info')
+			
+			/* Animation Variables */
 			var view2 = document.querySelector('#view-2');
 			var view4 = document.querySelector('#view-4');
 			var view5 = document.querySelector('#view-5');
-			view4.style.right="-100%";
-			view4.style.opacity="0";
-			
-			view2.classList.toggle('hidden');
 
-			setTimeout(function(){ 
-				view4.classList.toggle('hidden');
-				view5.style.right="0";
-				//view5.classList.toggle('hidden');
-			}, 100);
+			if( firstName == null || lastName  ==  null || addr1 ==  null || city  ==  null || zip  ==  null || state  ==  null || email  ==  null || state  ==  "State"){
+				errorMsg.className = 'fadeIn red';
+			} else {
+				view4.style.right="-100%";
+				view4.style.opacity="0";
+				
+				view2.classList.toggle('hidden');
+
+				setTimeout(function(){ 
+					view4.classList.toggle('hidden');
+					view5.style.right="0";
+					//view5.classList.toggle('hidden');
+				}, 100);
+			}
 
 		}
 			
